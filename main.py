59226e945c5d7f91d95b11192653e5edbce64a50
Jeong-Je <131790445+Jeong-Je@users.noreply.github.com>
2023-10-06 15:09:41 +0900

    블로그 RSS 크롤링 🔎

--- main.py
+++ main.py
@@ -5,14 +5,9 @@ RSS_FEED = feedparser.parse(URL)
 MAX_POST = 5
 
 markdown_text = """
-# Jeong Je Son 🖐️
-
-[![Solved.ac Profile](http://mazassumnida.wtf/api/v2/generate_badge?boj=castolrz)](https://solved.ac/castolrz/)
-![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=Jeong-Je&layout=compact&theme=ruvbox) <br />
-[![Solved.ac프로필](http://mazassumnida.wtf/api/mini/generate_badge?boj=castolrz)](https://solved.ac/castolrz/)
-
-## Skills
-![TypeScript](https://img.shields.io/badge/typescript-3178C6.svg?&style=for-the-badge&logo=typescript&logoColor=white)
+![header](https://capsule-render.vercel.app/api?type=waving&color=auto&height=300&section=header&text=Jeong%20Je&fontSize=90&fontColor=ffffff&animation=fadeIn&fontAlignY=38&descAlignY=51&descAlign=62)
+## 💪 Skills
+![TypeScript](https://img.shields.io/badge/TypeScript-3178C6.svg?&style=for-the-badge&logo=typescript&logoColor=white)
 ![JavaScript](https://img.shields.io/badge/javascript-F7DF1E.svg?&style=for-the-badge&logo=javascript&logoColor=white)
 ![C++](https://img.shields.io/badge/C++-00599C.svg?&style=for-the-badge&logo=cplusplus&logoColor=white)
 <br />
@@ -20,9 +15,13 @@ markdown_text = """
 ![nestjs](https://img.shields.io/badge/NestJS-E0234E.svg?&style=for-the-badge&logo=NestJS&logoColor=white)
 ![Next.js](https://img.shields.io/badge/Next.js-000000.svg?&style=for-the-badge&logo=nextdotjs&logoColor=white)
 
-## My Blog
+## ♾️ Algorithm
+### BOJ
+[![Solved.ac Profile](http://mazassumnida.wtf/api/v2/generate_badge?boj=castolrz)](https://solved.ac/castolrz/) <br/>
+[![Solved.ac프로필](http://mazassumnida.wtf/api/mini/generate_badge?boj=castolrz)](https://solved.ac/castolrz/)
+## 📚 My Blog
 <a href="https://jeongje.vercel.app/" target='_blank'><img src="https://img.shields.io/badge/myblog-000000?style=for-the-badge&logo=nextdotjs&logoColor=white"></a>
-## 📒 Latest Blog Post
+### 📒 Latest Blog Post
 
 """  # list of blog posts will be appended here
 
